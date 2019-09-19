c87b21204dbd43461f4a38f5df7341a07f773d1d
Christopher Porter <peng.hao.zhi@gmail.com>
2019-09-19 10:07:49 -0600

    In Progress

--- Exam_1/Exam_1.R
+++ Exam_1/Exam_1.R
@@ -1,4 +1,5 @@
 dna <- read.csv("DNA_Conc_by_Extraction_Date.csv")
+dna[,"Year_Collected"] <- factor(dna[,"Year_Collected"])
 attach(dna)
 
 #Create Histograms for Katy & Ben
@@ -6,8 +7,19 @@ hist(DNA_Concentration_Ben, xlab = "DNA Concentration", ylab = "Number of Sample
 hist(DNA_Concentration_Katy, xlab = "DNA Concentration", ylab = "Number of Samples", main = "Katy's Results")
 
 #Create Boxplots for Katy & Ben by Year
-year <- as.factor(dna$Year_Collected)
-dna <- as.data.frame()
 ben.year <- dna[, col <- c("Year_Collected", "DNA_Concentration_Ben")] 
 katy.year <- dna[, col <- c("Year_Collected", "DNA_Concentration_Katy")]
-boxplot(ben.year, x= ben.year$Year_Collected, y= ben.year$DNA_Concentration_Ben, xlab = "YEAR", ylab = "DNA Concentration", main = "Ben's Extractions")
+plot(ben.year, xlab = "YEAR", ylab = "DNA Concentration", main = "Ben's Extractions")
+plot(katy.year, xlab = "YEAR", ylab = "DNA Concentration", main = "Katy's Extractions")
+
+# Save Boxplots as Jpeg
+jpeg("Ben_Boxplot.jpg")
+plot(ben.year, xlab = "YEAR", ylab = "DNA Concentration", main = "Ben's Extractions")
+dev.off()
+
+jpeg("Katy_Boxplot.jpg")
+plot(katy.year, xlab = "YEAR", ylab = "DNA Concentration", main = "Katy's Extractions")
+dev.off()
+
+#Compare Ben & Katy Extractions by Year
+
