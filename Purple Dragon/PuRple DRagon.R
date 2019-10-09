65ae900b0f76fd40ab57941ba9610ef4b1b908a2
Christopher Porter <peng.hao.zhi@gmail.com>
2019-10-09 15:03:27 -0600

    New panel.background

--- Purple Dragon/PuRple DRagon.R
+++ Purple Dragon/PuRple DRagon.R
@@ -1,11 +1,20 @@
+#Install Packages
+install.packages("ggplot2")
+install.packages("png")
+
 #Library Call
 library(ggplot2)
+library(png)
+library(grid)
 
 #read in data
 data(iris)
 
+image <- readPNG("./Purple Dragon/purple_dragon.PNG")
+
 #Create Plot
 ggplot(data = iris, aes(x= Petal.Length, y= Sepal.Length, color = Species)) +
+  annotation_custom(rasterGrob(image, width = unit(1,"npc"), height = unit(1,"npc")), -Inf, Inf, -Inf, Inf) +
   geom_point(size = 25)+
   geom_smooth()+
   theme(plot.title = element_text(size = 36),
@@ -13,7 +22,6 @@ ggplot(data = iris, aes(x= Petal.Length, y= Sepal.Length, color = Species)) +
         legend.box.background = element_rect(fill = '#9934eb'),
         legend.key = element_rect(fill = '#9934eb'),
         plot.background = element_rect(fill = 'purple', colour = 'purple'),
-        panel.background = element_rect(fill = '#d634eb'),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) +
   labs(title = "Chart",
