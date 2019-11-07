aeda94d80595d5f2d6c2c0e3ca0c17cd134efe26
Christopher Porter <peng.hao.zhi@gmail.com>
2019-11-07 12:31:41 -0700

    Mapping Update

--- Final Project/Mapping.R
+++ Final Project/Mapping.R
@@ -1,15 +1,30 @@
 #Library Calls
 library(tidyverse)
 library(ggmap)
-library(colmaps)
+library(rgdal)
 
+key <- "AIzaSyAFtAiA0BypI0EnnaunaG9weqiV9Cl7s3k"
+
+#Register Google Maps API Key
+register_google(key = key)
+
+#Install ColMaps####
 install.packages("devtools")
 devtools::install_github("nebulae-co/colmaps")
 
-3#Register Google Maps API Key
-register_google(key = "AIzaSyAFtAiA0BypI0EnnaunaG9weqiV9Cl7s3k")
+#Create Colombia Map ####
 COL.center <- 
 COL.box <- c(left = -79.302364, bottom = -4.709750, right = -64.682794, top = 13.225237)
-map.COL <- get_googlemap(center = c(lon = -72.976339, lat = 4.299023), maptype = "hybrid", source = "google", force = T)
+map.COL <- get_map(location = COL.box, maptype= "roadmap", color = "bw", force = T)
+gmap.COL <- get_googlemap(center = c(lon = -72.976339, lat = 4.299023),zoom = 5, maptype = "roadmap", source = "google", force = T)
+
 ggmap(map.COL)
 ?get_googlemap
+#Load SHP file data
+library(rgdal)
+shpData <- readOGR(dsn="./images/mpio_84.shp")
+proj4string(shpData) # describes data’s current coordinate reference system
+# to change to correct projection:
+shpData <- spTransform(shpData,
+                       CRS("+proj=longlat +datum=WGS84")) 
+ogrListLayers(dsn = "./images/mpio_84.prj")
